d673aa223b115aa91a6c0e30b964b5e178419afd
Jeff Chang <jeffrey.chang@twotalltotems.com>
2018-02-01 12:02:34 -0800

    Initial Commit

---
+++ Manulife Account/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Manulife Account
+//
+//  Created by Jeff Chang on 2018-02-01.
+//  Copyright © 2018 jeff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
